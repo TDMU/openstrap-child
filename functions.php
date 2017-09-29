19297cc780e57ff67a3346dc334dab8f29b95887
Andriy Semenets <semteacher@gmail.com>
2017-09-29 19:51:04 +0300

    styles - experiment 2

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'openstrap-style'; // This is 'openstrap-style' for the Openstrap theme.
+    //$parent_style = 'openstrap-style'; // This is 'openstrap-style' for the Openstrap theme.
+	$parent_style = 'parent-style'; // This is 'openstrap-style' for the Openstrap theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'openstrap-child-style',
+    wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
