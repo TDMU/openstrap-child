9581d954a35d662ac9b0b2f2c71ded3a4cb90ae8
Andriy Semenets <semteacher@gmail.com>
2017-10-01 11:56:45 +0300

    experiment 2 - ok! - fixed styles calling in functions.php - re-implement basic site layout

--- functions.php
+++ functions.php
@@ -1,7 +1,6 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    //$parent_style = 'openstrap-style'; // This is 'openstrap-style' for the Openstrap theme.
 	$parent_style = 'parent-style'; // This is 'openstrap-style' for the Openstrap theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
