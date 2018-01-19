95e4b7ad53b0dbbd87261f67ae371d55d981f494
Andriy Semenets <semteacher@gmail.com>
2018-01-19 22:08:22 +0200

    create function to load required JS libraries
    # Conflicts:
    #	functions.php

--- functions.php
+++ functions.php
@@ -16,4 +16,23 @@ function openstrap_child_theme_setup() {
     load_child_theme_textdomain( 'openstrap', get_template_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'openstrap_child_theme_setup' );
+
+function theme_js() {
+    //load Google Maps API library
+	wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDpGoWzDDmQ-a6geOEVZ32VQXWZ2jmLoSs', array(), null, null);
+	
+	//If necessary - forece required jQuery version
+    //wp_deregister_script('jquery');
+    //wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', array(), null, null);
+
+    //load LOCAL COPY of jQuiry-UI
+	wp_enqueue_script('custom-jqueryui', get_stylesheet_directory_uri() . '/js/jquery-ui.min.js', array(), null, null);
+	
+	//load custom interface
+    wp_enqueue_script( 'tour_js', get_stylesheet_directory_uri() . '/js/tour.js', array( 'jquery','custom-jqueryui','google-maps' ), '1.0', null );
+	//provide link to resources (img/css) that being used in JS
+    $translation_array = array( 'templateUrl' => get_stylesheet_directory_uri() );
+    wp_localize_script( 'tour_js', 'theme_path', $translation_array );
+}
+add_action('wp_enqueue_scripts', 'theme_js');
 ?>
