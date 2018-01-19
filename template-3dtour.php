6115f5d64eb8798f8e17230b3632438a1c4c7d24
Andriy Semenets <semteacher@gmail.com>
2018-01-19 22:32:43 +0200

    page template with 3d-tour has been added

---
+++ template-3dtour.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: TSMU 3D Tours on Google Maps
+ *
+ * Page template for
+ *
+ * @package Openstrap
+ * @since Opentrap_child
+ */
+
+get_header(); ?>
+
+<?php $col =  openstrap_get_content_cols(); ?>
+
+	<!-- Main Content -->	
+	<div class="col-md-<?php echo $col;?>" role="content">
+		<div id="primary" class="site-content">
+			<div id="content" role="main">
+				<!-- 3D tour content -->
+				<h1>3D-тур корпусами ТДМУ</h1>
+				<div>
+					<h1 class="site-title" style="margin-bottom: 20px;"></h1>
+					<div id="map-canvas" style="width: 100% !important; height: 600px !important;"></div>
+					<div id="dialog-sw-canvas"></div>
+				</div>
+				<!-- End of 3D tour content -->
+			</div><!-- #content -->
+		</div><!-- #primary -->
+	</div><!-- .col-md-<?php echo $col;?> -->
+	<!-- End Main Content -->	
+<?php get_sidebar(); ?>	
+<?php get_footer(); ?>
